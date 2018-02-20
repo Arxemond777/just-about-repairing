9ec4378cef24663ebd79cd71e84349ddf9b355ab
Yuri Glushenkov <1arxemond1@gmail.com>
2018-02-20 17:03:03 +0300

    AdminProstooremonteBundle has been create and make some refactoring

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new ProstooremonteBundle\ProstooremonteBundle(),
+            new AdminProstooremonteBundle\AdminProstooremonteBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
